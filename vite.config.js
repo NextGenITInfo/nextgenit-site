81ac23772f659b1bbd49ebe42a9a3adc1ec41952
Eve0028 <62481375+Eve0028@users.noreply.github.com>
2025-10-09 21:34:11 +0200

    chore: minify - esbuild

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './', // Use relative paths for Cloudflare Pages
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
     assetsDir: 'assets',
-    sourcemap: false
+    sourcemap: false,
+    minify: 'esbuild'
   },
   server: {
     port: 3000,
